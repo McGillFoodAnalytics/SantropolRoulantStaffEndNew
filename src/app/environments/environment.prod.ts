eca07b77a2d574cb96a95a369536d8fb5b68a639
= <=>
2020-05-17 20:05:20 -0400

    Liftoff

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const AppConfig = {
+  production: true,
+  environment: 'PROD',
+  firebase: {
+    apiKey: "AIzaSyBTiUXyilIjxNRhMd_DOdefWHT7-YFqfso",
+    authDomain: "santropolroulant-b4d14.firebaseapp.com",
+    databaseURL: "https://santropolroulant-b4d14.firebaseio.com",
+    projectId: "santropolroulant-b4d14",
+    storageBucket: "santropolroulant-b4d14.appspot.com",
+    messagingSenderId: "730863709360"
+  }
+};
