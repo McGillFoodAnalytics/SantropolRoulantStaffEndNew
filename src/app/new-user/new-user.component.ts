0fcd91703380317762a3b56b4f6dbbf120c6196c
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2021-06-04 13:48:39 -0400

    Add user using auth (does not fully work yet)

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -5,6 +5,8 @@ import { User } from '../shared/models/user';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { formatDate } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {firebase} from '@firebase/app';
+import '@firebase/auth';
 
 
 @Component({
@@ -48,29 +50,54 @@ export class NewUserComponent implements OnInit {
     this.modalReference = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'lg'});
   }
 
+  createAuthUser(email: any): any {
+    // Remember current user
+    let originalUser = firebase.auth().currentUser
+
+    // Create new user, will automatically sign in
+    firebase.auth().createUserWithEmailAndPassword(email, email)
+    .then((userCredential) => {
+      // Signed in 
+      var key =  userCredential.user.uid
+      console.log(key);
+      console.log(originalUser)
+      return key
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage)
+      // ..
+    });
+  }
+
 
   newUser(user: any): void {
-  user.id = user.last_name.charAt(0).toLowerCase() + user.last_name.charAt(1).toLowerCase() + user.phone_number;
-  this.db.object('/user/' + user.id)
-    .update({
-      address_city: user.address_city,
-      address_number: user.address_number,
-      address_postal_code: user.address_postal_code,
-      address_street: user.address_street,
-      dob: user.dob,
-      email: user.email,
-      first_name: user.first_name,
-      key: user.id,
-      active_status: true,
-      last_name: user.last_name,
-      no_show: 0,
-      phone_number: user.phone_number,
-      emergency_contact_number: user.emergency_contact_number,
-      emergency_contact_name: user.emergency_contact_name,
-      emergency_relationship: user.emergency_relationship,
-      signup_date: formatDate(new Date(), 'yy/MM/dd', 'en'),
-      cancellations: 0
-     });
+    user.id = user.last_name.charAt(0).toLowerCase() + user.last_name.charAt(1).toLowerCase() + user.phone_number;
+
+    var key = this.createAuthUser(user.email)
+
+    this.db.object('/user/' + user.id)
+      .update({
+        address_city: user.address_city,
+        address_number: user.address_number,
+        address_postal_code: user.address_postal_code,
+        address_street: user.address_street,
+        dob: user.dob,
+        email: user.email,
+        first_name: user.first_name,
+        key: key,
+        active_status: true,
+        last_name: user.last_name,
+        no_show: 0,
+        phone_number: user.phone_number,
+        emergency_contact_number: user.emergency_contact_number,
+        emergency_contact_name: user.emergency_contact_name,
+        emergency_relationship: user.emergency_relationship,
+        signup_date: formatDate(new Date(), 'yy/MM/dd', 'en'),
+        cancellations: 0
+      });
   }
 
   onSubmit(f){
