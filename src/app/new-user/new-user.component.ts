108d72f342d01a06c1bc7ffedadce45e9fd8ff03
Benjamin Emiliani <49131189+BenjaminEmiliani@users.noreply.github.com>
2021-06-04 15:57:31 -0400

    Revert "Added auth to new vols"

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -5,8 +5,6 @@ import { User } from '../shared/models/user';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { formatDate } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {firebase} from '@firebase/app';
-import '@firebase/auth';
 
 
 @Component({
@@ -50,46 +48,29 @@ export class NewUserComponent implements OnInit {
     this.modalReference = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'lg'});
   }
 
-  newUser(user: any): void {
-    user.id = user.last_name.charAt(0).toLowerCase() + user.last_name.charAt(1).toLowerCase() + user.phone_number;
-    
-    // Create a dummy app so that current user is not signed out 
-    let authWorkerApp = firebase.initializeApp(firebase.app().options, 'auth-worker');
-    let authWorkerAuth = firebase.auth(authWorkerApp);
-    authWorkerAuth.setPersistence(firebase.auth.Auth.Persistence.NONE); // disables caching of account credentials
 
-        // Use the dummy app to create a new user
-        authWorkerAuth.createUserWithEmailAndPassword(user.email, user.email)
-        .then((userCredential) => {
-          // Signed in 
-          console.log(userCredential.user.uid);
-          this.db.object('/user/' + user.id)
-          .update({
-              address_city: user.address_city,
-              address_number: user.address_number,
-              address_postal_code: user.address_postal_code,
-              address_street: user.address_street,
-              dob: user.dob,
-              email: user.email,
-              first_name: user.first_name,
-              key: userCredential.user.uid, // Firebase User UID
-              active_status: true,
-              last_name: user.last_name,
-              no_show: 0,
-              phone_number: user.phone_number,
-              emergency_contact_number: user.emergency_contact_number,
-              emergency_contact_name: user.emergency_contact_name,
-              emergency_relationship: user.emergency_relationship,
-              signup_date: formatDate(new Date(), 'yy/MM/dd', 'en'),
-              cancellations: 0
-          })
-          //console.log(originalUser)
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log(errorMessage)
-        });
+  newUser(user: any): void {
+  user.id = user.last_name.charAt(0).toLowerCase() + user.last_name.charAt(1).toLowerCase() + user.phone_number;
+  this.db.object('/user/' + user.id)
+    .update({
+      address_city: user.address_city,
+      address_number: user.address_number,
+      address_postal_code: user.address_postal_code,
+      address_street: user.address_street,
+      dob: user.dob,
+      email: user.email,
+      first_name: user.first_name,
+      key: user.id,
+      active_status: true,
+      last_name: user.last_name,
+      no_show: 0,
+      phone_number: user.phone_number,
+      emergency_contact_number: user.emergency_contact_number,
+      emergency_contact_name: user.emergency_contact_name,
+      emergency_relationship: user.emergency_relationship,
+      signup_date: formatDate(new Date(), 'yy/MM/dd', 'en'),
+      cancellations: 0
+     });
   }
 
   onSubmit(f){
