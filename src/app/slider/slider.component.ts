04ba014a341a477c664b9ca46f1cb74f58778aae
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-06-09 14:24:05 -0400

    Added 4th week to Schedule

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -8,14 +8,15 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('slide', [
       state('first', style({ transform: 'translateX(0)' })),
-      state('second', style({ transform: 'translateX(-33.33%)' })),
-      state('third', style({ transform: 'translateX(-66.666%)' })),
+      state('second', style({ transform: 'translateX(-25%)' })),
+      state('third', style({ transform: 'translateX(-50%)' })),
+      state('fourth', style({ transform: 'translateX(-75%)' })),
       transition('* => *', animate(300))
     ])
-  ]
+  ] 
 })
 export class SliderComponent {
   @Input() activePane: PaneType = 'third';
 }
 
-type PaneType = 'first' | 'second' | 'third';
+type PaneType = 'first' | 'second' | 'third' | 'fourth';
