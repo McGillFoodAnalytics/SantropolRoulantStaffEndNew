5322e326808e66abae15fd9183b16da9f5b753c0
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-06-30 10:42:02 -0400

    small window size fix

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -316,7 +316,7 @@ if (this.auth.isSignInWithEmailLink(window.location.href)) {
 
   open(){
 
-    const modalRef = this.m.open(ManageAccountComponent, {size: 'sm', centered: true});
+    const modalRef = this.m.open(ManageAccountComponent, {size: 'md', centered: true});
     modalRef.result.then(user => {
 
     })
