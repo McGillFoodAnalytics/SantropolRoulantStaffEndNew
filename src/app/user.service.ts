9395f931c5672613f542e46851de02e2f0c39634
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-08-20 15:24:09 -0400

    update user prof (not done)

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,59 +1,60 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
-export type CreateUserRequest = { displayName: string, email: string, role: string, phoneNumber: string }
-export type UpdateUserRequest = { uid: string} & CreateUserRequest
+export type CreateUserRequest = {
+  displayName: string;
+  email: string;
+  role: string;
+  phoneNumber: string;
+};
+export type UpdateUserRequest = { uid: string } & CreateUserRequest;
 
 @Injectable({
- providedIn: 'root'
+  providedIn: "root",
 })
 export class UserService {
+  private baseUrl =
+    "https://us-central1-santropolroulant-b4d14.cloudfunctions.net/api/users";
 
- private baseUrl = 'https://us-central1-santropolroulant-b4d14.cloudfunctions.net/api/users'
-
- constructor(
-   private http: HttpClient,
- ) { }
+  constructor(private http: HttpClient) {}
   get users$(): Observable<any[]> {
-   return this.http.get<{ users: any[] }>(`${this.baseUrl}`).pipe(
-     map(result => {
-       return result.users
-     })
-   )
- }
+    return this.http.get<{ users: any[] }>(`${this.baseUrl}`).pipe(
+      map((result) => {
+        return result.users;
+      })
+    );
+  }
 
- user$(id: string): Observable<any> {
-   return this.http.get<{ user: any }>(`${this.baseUrl}/${id}`).pipe(
-     map(result => {
-       return result.user
-     })
-   )
- }
+  user$(id: string): Observable<any> {
+    return this.http.get<{ user: any }>(`${this.baseUrl}/${id}`).pipe(
+      map((result) => {
+        return result.user;
+      })
+    );
+  }
 
- create(user: CreateUserRequest) {
-   return this.http.post(`${this.baseUrl}`, user)
- }
+  create(user: CreateUserRequest) {
+    return this.http.post(`${this.baseUrl}`, user);
+  }
 
- edit(user: UpdateUserRequest) {
+  edit(user: UpdateUserRequest) {
+    console.log("update request");
+    console.log(user);
+    return this.http.patch(`${this.baseUrl}/${user.uid}`, user);
+  }
 
-  console.log("update request")
-  console.log(user)
+  delete(uid) {
+    return this.http.delete(`${this.baseUrl}/${uid}`);
+  }
 
-   return this.http.patch(`${this.baseUrl}/${user.uid}`, user)
- }
+  passwordReset(id) {
+    return this.http.patch(`${this.baseUrl}/auth/${id}`, null);
+  }
 
- delete(uid){
-   return this.http.delete(`${this.baseUrl}/${uid}`)
- }
-
- passwordReset(id){
-
-   return this.http.patch(`${this.baseUrl}/auth/${id}`, null)
-
- }
-
-
-
+  findRecordIdByUser(uid) {
+    console.log("finding record for: " + uid)
+    //return this.http.get(`${this.baseUrl}/airtable/${uid}`);
+  }
 }
